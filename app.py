fcb6bbd9aa4f71b57eed5bc6a72e03fba5bf753e
AbyTed <119811665+AbyTed@users.noreply.github.com>
2024-07-28 21:50:13 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify # type: ignore
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from flask_migrate import Migrate # type: ignore
+from werkzeug.security import generate_password_hash, check_password_hash # type: ignore
+from flask_cors import  CORS 
+
+app = Flask(__name__)
+
+# SQLite database configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+@app.route('/', methods=['GET'])
+def test():
+    return jsonify({'message': 'Flask server is connected!'})
+
+@app.route("/api/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    username = data["username"]
+    email = data["email"]
+    password = generate_password_hash(data["password"], method="sha256")
+
+    new_user = User(username=username, email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User signed up successfully"}), 201
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    email = data["email"]
+    password = data["password"]
+
+    user = User.query.filter_by(email=email).first()
+    if user and check_password_hash(user.password, password):
+        return jsonify({"message": "Login successful"}), 200
+    else:
+        return jsonify({"message": "Invalid email or password"}), 401
+
+@app.route("/shark", methods=["GET"])
+def shark():
+    return ("Shark")
+
+
+
+
+
+CORS(app, resources={r"/*":{'origins':"*"}})
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
